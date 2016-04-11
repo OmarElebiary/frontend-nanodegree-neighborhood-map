ea407e2bd3bc5854aba91fcd49348ea44ebf41a2
Omar <es-omar.khairy1217@alexu.edu.eg>
2016-04-11 17:57:45 +0200

    Added files via upload

---
+++ js/data.js
@@ -0,0 +1,121 @@
+var locationData = [{
+	name: 'Trattoria Trecolori',
+	yelpID: 'trattoria-trecolori-new-york',
+	address: '254 W 47th St, New York, NY 10036',
+	location: {
+		lat: 40.7599106,
+		lng: -73.98676160000002
+	}
+}, {
+	name: 'Havana Central',
+	yelpID: 'havana-central-new-york-6',
+	address: '151 W 46th St, New York, NY 10036',
+	location: {
+		lat: 40.7584036, 
+		lng: -73.98407989999998
+	}
+}, {
+	name: 'The Long Room',
+	yelpID: 'the-long-room-new-york',
+	address: '120 W 44th St, New York, NY 10036',
+	location: {
+		lat: 40.7564205, 
+		lng: -73.98428849999999
+	}
+}, {
+	name: 'The Lambs Club',
+	yelpID: 'the-lambs-club-new-york',
+	address: '337 E 1st St Los Angeles, CA 90012',
+	location: {
+		lat: 40.7565107,
+		lng: -73.98464109999998
+	}
+}, {
+	name: 'Brooklyn Diner Times Square',
+	yelpID: 'brooklyn-diner-times-square-new-york',
+	address: '155 W 43rd St, New York, NY 10036',
+	location: {
+		lat: 40.7565435,
+		lng: -73.9856039
+	}
+}, {
+	name: "Dave & Buster's",
+	yelpID: 'dave-and-busters-new-york',
+	address: '234 W 42nd St, New York, NY 10036',
+	location: {
+		lat: 40.7565811, 
+		lng: -73.9886598
+	}
+}, {
+	name: "Guy's American Kitchen & Bar",
+	yelpID: 'guys-american-kitchen-and-bar-new-york-2',
+	address: '220 W 44th St, New York, NY 10036',
+	location: {
+		lat: 40.757658, 
+		lng: -73.98727280000003
+	}
+}, {
+	name: 'Planet Hollywood',
+	yelpID: 'planet-hollywood-new-york',
+	address: '1540 Broadway, New York, NY 10036',
+	location: {
+		lat: 40.758174,
+		lng: -73.98512900000003
+	}
+}, {
+	name: "Del Frisco’s Double Eagle Steak House",
+	yelpID: 'del-friscos-double-eagle-steak-house-new-york',
+	address: '1221 Avenue of the Americas, New York, NY 10020',
+	location: {
+		lat: 40.75947619999999, 
+		lng: -73.98205080000002
+	}
+}, {
+	name: 'Connolly’s Pub & Restaurant',
+	yelpID: 'connollys-pub-and-restaurant-new-york-5',
+	address: '14 E 47th St, New York, NY 10017',
+	location: {
+		lat: 40.75733470000001, 
+		lng: -73.98362600000002
+	}
+}, {
+	name: 'Sake Bar Hagi',
+	yelpID: 'sake-bar-hagi-new-york',
+	address: '152 W 49th St, New York, NY 10019',
+	location: {
+		lat: 40.7601441, 
+		lng: -73.98322410000003
+	}
+},{
+	name: 'Forty Four Lounge',
+	yelpID: 'forty-four-lounge-new-york',
+	address: 'Royalton Hotel, 44 W 44th St, New York, NY 10036',
+	location: {
+		lat: 40.75558100000001, 
+		lng: -73.98222599999997
+	}
+},{
+	name: 'Blue Fin',
+	yelpID: 'blue-fin-new-york',
+	address: 'W Hotel Times Square, 1567 Broadway, New York, NY 10036',
+	location: {
+		lat: 40.7593299, 
+		lng: -73.98572209999998
+	}
+},{
+	name: 'Courtyard New York Manhattan/Times Square West',
+	yelpID: 'courtyard-new-york-manhattan-times-square-west-new-york',
+	address: '307 West 37th Street West 37th Street 8th Ave, New York, NY 10018',
+	location: {
+		lat: 40.75347759999999, 
+		lng: -73.98617150000001
+	}
+},{
+	name: '48 Lounge',
+	yelpID: '48-lounge-new-york',
+	address: '1221 Ave Of The Americas, New York, NY 10020',
+	location: {
+		lat: 40.7593334, 
+		lng: -73.9823346
+	}
+}];
